7cf0d6fff1c9f720c73e4d6bb88d3393b36a7255
Farzad Tabashir <ftabashir@gmail.com>
2019-09-13 15:11:52 +0430

    set fa-IR locale

--- lib/room.dart
+++ lib/room.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:otaghak_guest/repository/model/index-response.dart' as models;
 
 class RoomWidget extends StatelessWidget {
@@ -63,6 +64,13 @@ class IndexWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('fa', 'IR'),
+      ],
       locale: Locale('fa', 'IR'),
       title: 'Fetch Data Example',
       theme: ThemeData(
