81b97a74dd5d5e231b5ca5407ca5aac1efd8e4eb
Farzad Tabashir <ftabashir@gmail.com>
2019-09-15 01:39:28 +0430

    fix some room dimensions and font sizes

--- lib/widget/room.dart
+++ lib/widget/room.dart
@@ -9,49 +9,59 @@ class RoomWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      elevation: 4,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(2),
-            child: SizedBox(
-              width: 162,
-              height: 122,
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                child: FittedBox(
-                  fit: BoxFit.cover,
-                  child: Image.network(room.imageUrl),
+    return SizedBox(
+      width: 166,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        elevation: 4,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(2),
+              child: SizedBox(
+                width: 162,
+                height: 122,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: Image.network(room.imageUrl),
+                  ),
                 ),
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  room.title,
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Text(room.cityName),
-                Row(
-                  children: <Widget>[
-                    Text('${room.bedrooms} اتاق‌خواب'),
-                    Text('${room.personCapacity} نفر'),
-                  ],
-                ),
-                Text('از ${room.basePrice.toInt()} تومان'),
-              ],
-            ),
-          )
-        ],
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    room.title,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    room.cityName,
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text('${room.bedrooms} اتاق‌خواب',
+                          style: TextStyle(fontSize: 12)),
+                      Text('${room.personCapacity} نفر',
+                          style: TextStyle(fontSize: 12)),
+                    ],
+                  ),
+                  Text('از ${room.basePrice.toInt()} تومان',
+                      style: TextStyle(fontSize: 13)),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
