29dc2b7e3b381afd9e2610d73034c4b205aeb368
Farzad Tabashir <ftabashir@gmail.com>
2019-09-22 01:20:19 +0330

    use WillPopScope create co-operation between back button and navigators

--- lib/widget/home.dart
+++ lib/widget/home.dart
@@ -14,32 +14,35 @@ class HomeWidget extends StatefulWidget {
 }
 
 class HomeState extends State<HomeWidget> {
-  int _currentIndex = 0;
+  static const _DEFAULT_INDEX = 0;
+  int _currentIndex = _DEFAULT_INDEX;
 
-  final _homeNavKey = GlobalKey<NavigatorState>();
-  final _placeholderNavKey1 = GlobalKey<NavigatorState>();
-  final _placeholderNavKey2 = GlobalKey<NavigatorState>();
+  final _navKeys = [
+    GlobalKey<NavigatorState>(),
+    GlobalKey<NavigatorState>(),
+    GlobalKey<NavigatorState>()
+  ];
 
   var _children;
 
   HomeState() {
     _children = <Widget>[
       Navigator(
-        key: _homeNavKey,
+        key: _navKeys[0],
         onGenerateRoute: (route) => MaterialPageRoute(
           settings: route,
           builder: (context) => IndexWidget(index: fetchIndex()),
         ),
       ),
       Navigator(
-        key: _placeholderNavKey1,
+        key: _navKeys[1],
         onGenerateRoute: (route) => MaterialPageRoute(
           settings: route,
           builder: (context) => PlaceholderWidget(Colors.cyan),
         ),
       ),
       Navigator(
-        key: _placeholderNavKey2,
+        key: _navKeys[2],
         onGenerateRoute: (route) => MaterialPageRoute(
           settings: route,
           builder: (context) => PlaceholderWidget(Colors.teal),
@@ -67,33 +70,48 @@ class HomeState extends State<HomeWidget> {
       locale: Locale('fa', 'IR'),
       title: 'Fetch Data Example',
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'IranSans'),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Otaghak'),
-        ),
-        body: IndexedStack(
-          index: _currentIndex,
-          children: _children,
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          onTap: onTabTapped,
-          currentIndex: _currentIndex,
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(AppIcons.explore),
-              title: Text(AppStrings.explore),
+      home: WillPopScope(
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text('Otaghak'),
             ),
-            BottomNavigationBarItem(
-              icon: Icon(AppIcons.favorite),
-              title: Text(AppStrings.favorites),
+            body: IndexedStack(
+              index: _currentIndex,
+              children: _children,
             ),
-            BottomNavigationBarItem(
-              icon: Icon(AppIcons.profile),
-              title: Text(AppStrings.profile),
+            bottomNavigationBar: BottomNavigationBar(
+              onTap: onTabTapped,
+              currentIndex: _currentIndex,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(AppIcons.explore),
+                  title: Text(AppStrings.explore),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(AppIcons.favorite),
+                  title: Text(AppStrings.favorites),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(AppIcons.profile),
+                  title: Text(AppStrings.profile),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+          onWillPop: () async {
+            final navigator = _navKeys[_currentIndex].currentState;
+            if (navigator.canPop()) {
+              navigator.pop();
+              return false;
+            }
+            if (_currentIndex != _DEFAULT_INDEX) {
+              setState(() {
+                _currentIndex = _DEFAULT_INDEX;
+              });
+              return false;
+            }
+            return true;
+          }),
     );
   }
 }
